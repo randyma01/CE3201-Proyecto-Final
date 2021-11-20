51eb13321ec5953177668daf6753b78187d0cf09
kvn-95 <kevin.gonzalez0795@outlook.com>
2021-11-19 20:58:18 -0600

    Modulo Extend y Instr.Mem

--- poyecto-procesador/alu.sv
+++ poyecto-procesador/alu.sv
@@ -42,7 +42,98 @@ module alu #(parameter N=32)
 	logic [3:0]TFLAGS;
 
 
+  //"Instancias" de las operaciones 
+  
+	and_op AND_OP(src_A, src_B, temp_result_and, temp_flag_and); // AND
+	xor_op XOR_OP(src_A, src_B, temp_result_xor, temp_flag_xor); // XOR
+	sub_op SUB_OP(src_A, src_B, temp_result_sub, temp_flag_sub); // SUB
+	revsub_op REVSUB_OP(src_A, src_B, temp_result_rev_sub, temp_flag_rev_sub); // REVSUB
+	add_op ADD_OP(src_A, src_B, temp_result_sub, temp_flag_sub); // ADD
+	compare_op COMP_OP(src_A, src_B, temp_result_compare, temp_flag_compare); // COMPARE
+	compneg_op COMPNEG_OP(src_A, src_B, temp_result_comp_neg, temp_flag_comp_neg); // COMPARE NEG
+	or_op OR_OP(src_A, src_B, temp_result_or, temp_flag_or); // OR
+	move_op MOVE_OP(src_B, temp_result_move, temp_flag_move); // MOVE
+	sll_op SLL_OP(src_A, src_B, temp_result_sll, temp_flag_sll); // SLL
+	srl_op SRL_OP(src_A, src_B, temp_result_srl, temp_flag_srl); // SLL
+	clear_op CLEAR_OP(src_A, src_B, temp_result_clear, temp_flag_clear); // CLEAR
+	not_op NOT_OP(src_A, temp_result_not, temp_flag_not); // NOT
 
+  
+    always @(alu_ctrl)
+
+      begin	
+
+        case (alu_ctrl)
+          0 : begin 
+				   TRESULT = temp_result_and;
+				   TFLAGS = temp_flag_and;
+				  end
+
+          1 : begin 
+				   TRESULT = temp_result_xor;
+				   TFLAGS = temp_flag_xor;
+				  end
+		
+          2 : begin 
+				   TRESULT = temp_result_sub;
+				   TFLAGS = temp_flag_sub;
+				  end
+
+          3 : begin 
+				   TRESULT = temp_result_rev_sub;
+				   TFLAGS = temp_flag_rev_sub;
+				  end
+
+          4 : begin 
+				   TRESULT = temp_result_add;
+				   TFLAGS = temp_flag_add;
+				  end
+
+          6 : begin 
+				   TRESULT = temp_result_compare;
+				   TFLAGS = temp_flag_compare;
+				  end
+				  
+          7 : begin 
+				   TRESULT = temp_result_comp_neg;
+				   TFLAGS = temp_flag_comp_neg;
+				  end
+
+          8 : begin 
+				   TRESULT = temp_result_move;
+				   TFLAGS = temp_flag_move;
+				  end
+
+          9 : begin 
+				   TRESULT = temp_result_sll;
+				   TFLAGS = temp_flag_sll;
+				  end
+				
+				
+          10 : begin 
+				   TRESULT = temp_result_srl;
+				   TFLAGS = temp_flag_srl;
+				  end
+
+          11 : begin 
+				   TRESULT = temp_result_clear;
+				   TFLAGS = temp_flag_clear;
+				  end
+
+          12 : begin 
+				   TRESULT = temp_result_not;
+				   TFLAGS = temp_flag_not;
+				  end			  
+
+			 default : begin 
+				   TRESULT = 32'd0;
+				   TFLAGS = 4'b0000;
+				  end
+      endcase
+	  end
+	
+	assign alu_result = TRESULT;
+	assign alu_flags = TFLAGS;
 
 
 
--- poyecto-procesador/extend.sv
+++ poyecto-procesador/extend.sv
@@ -1,7 +1,45 @@
 module extend
 (
-
+	input logic [1:0] imm_src,
+	input logic [23:0] imm,
+	output logic [31:0] result
 	
 );
+	logic [7:0] temp_im0;
+	logic [11:0] temp_im1;
+	logic [32:0] temp_extend;
+
+	
+	always @(imm_src)
+		begin
+			
+			case(imm_src)
+			
+				0:
+					begin
+						 temp_im0 = imm[7:0];
+						 temp_extend ={24'd0, temp_im0};
+					end
+					
+				1:
+					begin
+						 temp_im1 = imm[11:0];
+						 temp_extend ={20'd0, temp_im1};
+					end
+					
+				2:
+					begin
+						 temp_extend= imm * 3'd4;
+					end
+				
+				default: temp_extend = 32'd0;
+					
+			endcase
+			
+		end
+
+	
+	
+	assign result = temp_extend;
 
 endmodule 
